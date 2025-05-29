4d910609fda487f0573e560302da1d1494ce8071
Kyrylo <beatxprod@gmail.com>
2025-05-29 22:03:20 +0300

    implement Eternal Background in GameManager

--- Assets/_GameData/_Script/Core/GameManager.cs
+++ Assets/_GameData/_Script/Core/GameManager.cs
@@ -1,16 +1,43 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+namespace Core
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class GameManager : MonoBehaviour
     {
-        
-    }
+        private const float distanceBetweenBackgrounds = 10.8f;
+        private int floorsCompleted;
 
-    // Update is called once per frame
-    void Update()
-    {
+        private List<GameObject> _backgrounds = new();
+        private Transform _camera;
         
+        private void Start()
+        {
+            foreach (Transform child in this.transform)
+            {
+                _backgrounds.Add(child.gameObject);
+            }
+            
+            _camera = GameObject.Find("Main Camera").transform;
+        }
+
+        private void Update()
+        {
+            int currentFloor = Mathf.FloorToInt(_camera.position.y / distanceBetweenBackgrounds);
+            
+            if (currentFloor > floorsCompleted)
+            {
+                TransitionToNextFloor();
+            }
+        }
+        
+        private void TransitionToNextFloor()
+        {
+            _backgrounds[0].transform.position += new Vector3(0f, distanceBetweenBackgrounds * 3, 0f);
+            _backgrounds.Add(_backgrounds[0]);
+            _backgrounds.RemoveAt(0);
+                
+            floorsCompleted++;
+        }
     }
 }
