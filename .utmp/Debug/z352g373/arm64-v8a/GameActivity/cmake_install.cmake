391372d428a61505a4001681158788e4823349f5
Kyrylo <beatxprod@gmail.com>
2025-06-22 22:05:07 +0300

    build update

--- .utmp/Debug/z352g373/arm64-v8a/GameActivity/cmake_install.cmake
+++ .utmp/Debug/z352g373/arm64-v8a/GameActivity/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Kirill/Desktop/UnityCourse/Lessons/Unity/UnityLesson2D/Library/PramData/AndroidApplicationController/Cache/com.DefaultCompany.UnityLesson2D/gradle-project/unityLibrary/src/main/cpp/GameActivity
+# Install script for directory: C:/Users/Kirill/Desktop/UnityCourse/Lessons/Unity/UnityLesson2D/Library/Bee/Android/Prj/IL2CPP/Gradle/unityLibrary/src/main/cpp/GameActivity
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
